c896ed78bb760d50cd9224922cfbf47e16f35d74
bensonrobert10 <bensonrobert10@gmail.com>
2014-03-30 18:02:41 +0100

    Added Solution

---
+++ PosTil/AL.PosTil.BL/BL_Common/PostilException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AL.PosTil.BL
+{
+    [global::System.Serializable]
+    public class PostilException : Exception
+    {
+        public PostilException() { }
+        public PostilException(string message) : base(message) { }
+        public PostilException(string message, Exception inner) : base(message, inner) { }
+        protected PostilException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ PosTil/AL.PosTil.DAL/DAL_Factory/DALFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AL.PosTil.DAL
+{
+    public abstract class DALFactory
+    {
+        public static IParcelInfo GetParcelInfoDAL()
+        {
+            return ParcelInfoDAL.Instance;
+        }
+    }
+}
